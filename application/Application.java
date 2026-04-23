4b29eff4d9961a9ff7ee9b8409442e3afbefdf5a
ianr02 <ianrenderos222@gmail.com>
2026-04-24 00:10:38 +0200

    Resgistration Form

---
+++ application/Application.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class RegistrationApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Load the FXML layout
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Registration.fxml"));
+        Parent root = loader.load();
+
+        // Set the title and the scene
+        primaryStage.setTitle("App Registration System");
+        primaryStage.setScene(new Scene(root, 400, 450));
+
+        // Display the window
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
